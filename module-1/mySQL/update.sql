6a5f73e176d6345f98f6a2671efa73552e119515
roosbot <roosrabenberg@gmail.com>
2020-05-12 17:34:44 +0200

    MySQL solution

---
+++ module-1/mySQL/update.sql
@@ -0,0 +1,15 @@
+UPDATE Salespersons
+SET StoreCountry = 'Miami'
+WHERE IDSalesperson = 5;
+
+UPDATE Customers
+SET Email = 'ppicasso@gmail.com'
+WHERE IDCustomer = 1;
+
+UPDATE Customers
+SET Email = 'lincoln@us.gov@gmail.com'
+WHERE IDCustomer = 2;
+
+UPDATE Customers
+SET Email = 'hello@napoleon.me'
+WHERE IDCustomer = 3;
